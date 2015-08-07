f08a79090638933b3fce0ac6e9ec9479d4294e0b
LiorUbuntu <liorshahverdi@gmail.com>
2015-08-07 14:37:24 -0700

    filesearch.py is actually pretty useful

--- chapter8/regexsearch/filesearch.py
+++ chapter8/regexsearch/filesearch.py
@@ -3,13 +3,25 @@ import sys, os, re
 print 'This program will search through all files in this directory for a match\nto the regex that you will provide.'
 print 'Enter a regex'
 regex_str = str(raw_input())
-regex = re.compile(regex_str)
+myregex = re.compile(regex_str)
 filenames = os.listdir('.')
+matches = []
 for filename in filenames:
 	if filename.endswith('.txt'):
 		# Open the file
 		myfile = open(filename, 'r')
+		# Extract the filetext as a list of string
 		text_lines = myfile.readlines()
-		matched_list = regex.findall(regex_str)
-		for match in matched_list:
-			print match
+		#iterate through each line
+		for line in text_lines:
+			#extract the words in the line
+			words = line.split()
+			#iterate through the words in the line
+			for word in words:
+				#word is a match, add it to list of matches
+				if myregex.search(word) is not None:
+					mo = myregex.search(word)
+					matches.append(mo.group())
+
+for match in matches:
+	print match
--- chapter8/regexsearch/search.py
+++ chapter8/regexsearch/search.py
@@ -1,4 +1,5 @@
 import re
 
 regex = re.compile('(\d+)')
-print ('Numbers found: '+str(regex.findall('There were 55000 people there tonight. Or maybe 750000')))
+print ('Numbers found: '+str(
+	regex.findall('There were 55000 people there tonight. Or maybe 750000')))
