684b4f7b7fc0b6313e4fe8bcf7ddd530c76b4229
LiorUbuntu <liorshahverdi@gmail.com>
2015-08-17 20:54:36 -0700

    i guess it once worked but now it kinda doesn't owell

--- chapter11/beautifulsoup/lucky.py
+++ chapter11/beautifulsoup/lucky.py
@@ -7,5 +7,10 @@ res = requests.get('http://google.com/search/q='+ ' '.join(sys.argv[1:]))
 res.raise_for_status()
 
 # TODO : Retrieve top search result links.
+soup = bs4.BeautifulSoup(res.text)
 
 # TODO : Open a browser tab for each result.
+linkElems = soup.select('.r a')
+numOpen = len(linkElems)
+for i in range(numOpen):
+	webbrowser.open('http://google.com/'+ linkElems[i].get('href'))
